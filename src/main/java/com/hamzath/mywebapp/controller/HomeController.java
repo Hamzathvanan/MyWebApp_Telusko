2f3748bb0734edbcc527d56ac1167254fb8983a7
Hamzathvanan <hamzath@arogya.life>
2023-07-04 01:06:05 +0530

    home.jsp HomeController Created

---
+++ src/main/java/com/hamzath/mywebapp/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.hamzath.mywebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/home") // Add the URL path ("/home")
+    public String home(){
+        System.out.println("Hi home!");
+        return "home.jsp";
+    }
+}
+
