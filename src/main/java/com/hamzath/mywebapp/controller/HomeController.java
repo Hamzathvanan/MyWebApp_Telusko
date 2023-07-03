fd4e1e812424d08faf2c09491ad843212b9add26
Hamzathvanan <hamzath@arogya.life>
2023-07-04 02:23:30 +0530

    added prefix and suffix

--- src/main/java/com/hamzath/mywebapp/controller/HomeController.java
+++ src/main/java/com/hamzath/mywebapp/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
     @RequestMapping("/home") // Add the URL path ("/home")
     public String home(){
         System.out.println("Hi home!");
-        return "home.jsp";
+        return "home";
     }
 }
 
